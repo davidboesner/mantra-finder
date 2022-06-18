f164442e157ee8c697ad44fc14e0926a1cc37821
David Boesner <david.boesner@dedalus.com>
2022-06-18 14:14:33 +0200

    use filter in autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +10,22 @@ import { FormControl } from '@angular/forms';
 })
 export class AppComponent {
   myControl = new FormControl();
-  options: string[] = ['Delhi', 'Mumbai', 'Banglore'];
+  options: string[] = ['Das zwölf Silben Mantra des Guru Rinpoche', 'Buddha des Grenzenlosen Lichts', 'Zuflucht zum Amitabha'];
+  filteredOptions: Observable<string[]>= this.myControl.valueChanges.pipe(
+    startWith(''),
+    map(value => this._filter(value || '')),
+  );
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }

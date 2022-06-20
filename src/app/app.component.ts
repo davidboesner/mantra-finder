fa8aa9ff06ffc5e6de5f2d128b566b890abc8cc5
David Boesner <david.boesner@dedalus.com>
2022-06-20 17:14:27 +0200

    Mantra finder findet Mantras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MAT_DATEPICKER_SCROLL_STRATEGY } from '@angular/material/datepicker';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 
@@ -9,8 +10,18 @@ import {map, startWith} from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title="Welches Mantra suchst du?";
+  displayVal:string="";
   myControl = new FormControl();
-  options: string[] = ['Das zwölf Silben Mantra des Guru Rinpoche', 'Buddha des Grenzenlosen Lichts', 'Zuflucht zum Amitabha'];
+  map = {};
+
+  map1 = new Map()
+  .set("ABC", "123")
+  .set("XYZ", "234");
+  
+  mapKeys:string[] = Array.from( this.map1.keys() );
+  
+  options: string[] = this.mapKeys;
   filteredOptions: Observable<string[]>= this.myControl.valueChanges.pipe(
     startWith(''),
     map(value => this._filter(value || '')),
@@ -28,4 +39,16 @@ export class AppComponent {
 
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
+
+  getValue(val:string) {
+     console.log("Hallo David: " + val);
+     console.log("this.mapKeys="  + typeof(this.mapKeys));
+     if (this.mapKeys.includes(val)) {
+      this.displayVal = val + ":" + this.map1.get(val);
+     } else {
+      this.displayVal = 'Kein Mantra gefunden für: ' + val;
+     }
+     
+  }
+
 }
